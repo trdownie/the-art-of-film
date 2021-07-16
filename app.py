6a8b17951a22348e8869d07af159f7a336b7ac69
trdownie <trdownie@gmail.com>
2021-07-16 11:00:59 +0000

    Added members area template and functionality, and connected it to login/register functions

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def index():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # 1) check whether username exists in db
+        # 1) CHECK WHETHER USERNAME EXISTS IN THE DB
         # a) define existing_user variable
         # b) get the (lowercase) username from the form
         # c) search 'users' collection to find first instance of username
@@ -36,25 +36,33 @@ def register():
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
-        # 2) if existing_user is Truthy (has a value), display error & reload
+        # 2) USERNAME ALREADY EXISTS
+        # a) if existing_user = Truthy (has a value)
         if existing_user:
+            # i) return error message
             flash(request.form.get("username")
                  + " already exists. Nice try, replicant.")
+            # ii) redirect to registration page
             return redirect(url_for("register"))
 
-        # 3) create a dictionary containing the user's (lowercase) username and password
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        # 4) insert the dictionary containing the user's details into the users collection
-        mongo.db.users.insert_one(register)
+        # 3) USERNAME DOESN'T EXIST
+        else:
+            # a) create a dictionary containing the user info
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+            # b) insert the dictionary into the users collection
+            mongo.db.users.insert_one(register)
 
-        # 5) add the newly created user into the session cookie & display success message
-        session["user"] = request.form.get("username").lower()
-        flash("Registration successful "
-                + request.form.get("username") 
-                + ". Welcome, human.")
+            # c) add the newly created user into the session cookie
+            session["member"] = request.form.get("username").lower()
+            # d) display registration success message
+            flash("Registration successful, "
+                    + request.form.get("username") 
+                    + ". Welcome, human.")
+            # e) redirect the member to their member's area
+            return redirect(url_for("members", member=session["member"]))
 
     return render_template("register.html")
 
@@ -62,20 +70,20 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # 1) check whether username exists in db (as above)
+        # 1) CHECK WHETHER USERNAME EXISTS (AS ABOVE)
         # (if found, existing_username is assigned user's document)
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
-        # 2) if existing_user exists, check the password
+        # 2) USERNAME EXISTS - CHECK PASSWORD
         if existing_user:
-            # a) check if hashed passwords match (input/db)
+            # a) hashed passwords match (input/db)
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                # i) passwords match, set session 'user'
-                session["user"] = request.form.get("username").lower()
-                # ii) return flash message welcoming user
-                flash("Welcome, " + request.form.get("username"))
+                        # i) set session variable 'member' to this username
+                        session["member"] = request.form.get("username").lower()
+                        # ii) send the member to their member's area
+                        return redirect(url_for("members", member=session["member"]))
             # b) passwords don't match
             else:
                 # i) return incorrect flash message
@@ -84,15 +92,26 @@ def login():
                 # ii) reload login page
                 return redirect(url_for("login"))
 
-        # 3) if username entered does not exist
+        # 3) USERNAME DOESN'T EXIST
         else:
             # a) return incorrect flash message
             flash("Username and/or password incorrect."
                     + " Thought Police dispatched.")
-            # ii) reload login page
+            # b) reload login page
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/members/<member>", methods=["GET", "POST"])
+def members(member):
+    # 1) DEFINE THE MEMBER VARIABLE
+    # a) take the session["member"] and find its instance in the db
+    # b) since this will return doc, only return the ["username"] attribute
+    member = mongo.db.users.find_one(
+        {"username": session["member"]})["username"]
+    # 2) RENDER MEMBERS TEMPLATE & PASSD IT MEMBER VARIABLE
+    return render_template("members.html", member=member)
 
 
 if __name__ == "__main__":
