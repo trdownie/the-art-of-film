950d374e3ecf2b1752a0241256e743c3d2fdde3e
trdownie <trdownie@gmail.com>
2021-07-28 10:24:20 +0000

    Refactoring and basic styling: about page & add/edit pages

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
 });
 
-
 // ENABLE TEXT AREA IN FORMS
 $('#textarea1').val('New Text');
     M.textareaAutoResize($('#textarea1'));
