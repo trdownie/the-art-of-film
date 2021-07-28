3653c33f0b3af616a1d3626d88978be5df5ceb80
trdownie <trdownie@gmail.com>
2021-07-28 09:09:44 +0000

    Removed tags from forms and JS file

--- static/js/script.js
+++ static/js/script.js
@@ -3,49 +3,8 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
 });
 
-// ADD_FILM > YEAR DATEPICKER
-$(document).ready(function(){
-    $('.datepicker').datepicker({format: "yyyy"});
-});
 
 // ENABLE TEXT AREA IN FORMS
 $('#textarea1').val('New Text');
     M.textareaAutoResize($('#textarea1'));
 
-// PLACEHOLDER FOR GENRE
-$('.chips-placeholder-1').chips({
-    placeholder: 'Genre',
-    secondaryPlaceholder: 'Add Another',
-});
-
-// PLACEHOLDER FOR TAGS
-$('.chips-placeholder-2').chips({
-    placeholder: 'Tags',
-    secondaryPlaceholder: 'Add Another',
-});
-
-// AUTOCOMPLETE FOR GENRE
-$('.chips-autocomplete-1').chips({
-    autocompleteOptions: {
-        data: {
-            'Action': null,
-            'Adventure': null,
-            'Crime': null
-        },
-        limit: Infinity,
-        minLength: 1
-    }
-  });
-
-// AUTOCOMPLETE FOR TAGS
-$('.chips-autocomplete-2').chips({
-    autocompleteOptions: {
-        data: {
-            'Emotional': null,
-            'Poignant': null,
-            'Surreal': null
-        },
-        limit: Infinity,
-        minLength: 1
-    }
-  });
